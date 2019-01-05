fc9fdce4d61762fa1d05e9a9636e3d634c0318ed
解闯 <chuang.xie@enai.im>
2019-01-05 11:48:24 +0800

    token与ticket缓存key

--- wxsign.go
+++ wxsign.go
@@ -5,12 +5,18 @@ type WxSign struct {
 	Appid string
 	// AppSecret 公众号秘钥
 	AppSecret string
+	// TokenRdsKey access_token缓存key
+	TokenRdsKey string
+	// TicketRdsKey ticket缓存key
+	TicketRdsKey string
 }
 
 // New 创建对象
-func New(appid, secret string) *WxSign {
+func New(appid, secret, tokenKey, ticketKey string) *WxSign {
 	return &WxSign{
-		Appid:     appid,
-		AppSecret: secret,
+		Appid:        appid,
+		AppSecret:    secret,
+		TokenRdsKey:  tokenKey,
+		TicketRdsKey: ticketKey,
 	}
 }
